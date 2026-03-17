3fc0ba172001ff67609fbea78dc7d42826347925
Syed Munib <syedmunibudden@gmail.com>
2026-03-17 01:12:26 -0500

    clean code

--- lib/features/ai_tools/view/ai_tools_view.dart
+++ lib/features/ai_tools/view/ai_tools_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:job_tracker/core/widget/drawer.dart';
+import 'package:job_tracker/features/ai_tools/widgets/build_tool_card.dart';
 
 class AIToolsView extends StatelessWidget {
   const AIToolsView({super.key});
@@ -35,78 +36,23 @@ class AIToolsView extends StatelessWidget {
 
             const SizedBox(height: 30),
 
-            _buildToolCard(
-              context,
-              title: "Resume Analyzer",
-              icon: Icons.description,
-              route: "/resume-analyzer",
-            ),
+            BuildToolCardAI(context: context, title: "Resume Analyzer", icon: Icons.description, route: "/resume-analyzer"),
 
             const SizedBox(height: 16),
 
-            _buildToolCard(
-              context,
-              title: "Job Description Analyzer",
-              icon: Icons.search,
-              route: "/job-analyzer",
-            ),
+            BuildToolCardAI(context: context, title: "Job Description Analyzer", icon: Icons.search, route: "/job-analyzer"),
 
             const SizedBox(height: 16),
 
-            _buildToolCard(
-              context,
-              title: "Cover Letter Generator",
-              icon: Icons.mail,
-              route: "/cover-letter",
-            ),
+            BuildToolCardAI(context: context, title: "Cover Letter Generator", icon: Icons.mail, route: "/cover-letter"),
 
             const SizedBox(height: 16),
 
-            _buildToolCard(
-              context,
-              title: "Interview Questions",
-              icon: Icons.question_answer,
-              route: "/interview-questions",
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildToolCard(
-      BuildContext context, {
-        required String title,
-        required IconData icon,
-        required String route,
-      }) {
-    return InkWell(
-      onTap: () {
-        Navigator.pushNamed(context, route);
-      },
-      borderRadius: BorderRadius.circular(12),
-      child: Container(
-        padding: const EdgeInsets.all(18),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12),
-          // color: Colors.grey.shade100,
-          border: Border.all(color: Colors.grey.shade300),
-        ),
-        child: Row(
-          children: [
-            Icon(icon, size: 28),
-            const SizedBox(width: 16),
-            Text(
-              title,
-              style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const Spacer(),
-            const Icon(Icons.arrow_forward_ios, size: 16)
+            BuildToolCardAI(context: context, title: "Interview Questions", icon: Icons.question_answer, route: "/interview-questions"),
           ],
         ),
       ),
     );
   }
 }
+
